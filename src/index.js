37ebbfd77bcdca3f3175f332d521b6640b5277cd
A-Tawfik <taamer@gmail.com>
2017-04-28 15:58:19 -0400

    add react tree and seed data

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Main from './components/Main/Main';
+// import Footer from './components/Footer/Footer';
 import './index.css';
 
 ReactDOM.render(
   <Main />,
   document.getElementById('root')
 );
+
+
+// ReactDOM.render(
+//   <Footer />,
+//   document.getElementById('root')
+// );
